d9130994df52515c5fbd5ea78d6fec0163aa98c8
Carson McKinstry <carson.mckinstry@gmail.com>
2018-11-10 23:13:15 -0600

    switched to prisma binding, instead of the ts-client

--- src/context.ts
+++ src/context.ts
@@ -1,9 +1,15 @@
 import { Context } from 'koa';
-import { prisma } from './generated/prisma-client/index';
+// import { prisma } from './generated/prisma-client/index';
+import { Prisma } from 'prisma-binding';
 
 const context = (req: { ctx: Context }) => ({
   ...req,
-  db: prisma
+  db: new Prisma({
+    typeDefs: 'src/generated/prisma.graphql',
+    endpoint: 'http://localhost:4466',
+    secret: 'mysecret123',
+    debug: true
+  })
 });
 
 export default context;
